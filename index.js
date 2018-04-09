dc5ffbc7b172c64c929af0b3bad2e6a88b8594fe
Nikolai Yakuschenko <nikolai.yakuschenko@gmail.com>
2018-04-09 22:56:14 +0300

    #3 Reject promise

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-var promise = new Promise(function(fulfill, reject){
-    setTimeout( function() {
-        return fulfill('FULFILLED!')
+var promise = new Promise(function (fulfill, reject) {
+    setTimeout(function(){
+        return reject(new Error('REJECTED!'));
     }, 300);
 });
 
-promise.then(function(result) {console.log(result)});
+function onReject (error) {
+    console.log(error.message);
+}
+
+promise.then(null, onReject);
+
+
